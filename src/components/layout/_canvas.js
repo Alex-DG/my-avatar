f8afc0a7513b27abe6f51ea07d64bbb04bdaab3c
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-04-02 13:50:15 +0100

    update style canvas

--- src/components/layout/_canvas.js
+++ src/components/layout/_canvas.js
@@ -19,6 +19,18 @@ const Bg = () => {
 const LCanvas = ({ children }) => {
   return (
     <Canvas
+      style={{
+        // position: 'absolute',
+        // top: '50%',
+        // left: '50%',
+        // margin: '0 auto',
+        // maxWidth: '500px',
+        // transform: 'translate(-50%, -50%)',
+        width: '100%',
+        height: '100%',
+        // border: 'solid 4px lightblue',
+        // borderRadius: '14px',
+      }}
       onCreated={({ events }) => {
         useStore.setState({ events })
       }}
